cb477b87f4e2487bda60d843bef2542645cb5f09
yehonatal <yonatanafewerk@gmail.com>
2025-04-08 19:08:29 +0300

    bug : vite config for base url reset to root

--- react-projects/task-todo-react/vite.config.ts
+++ react-projects/task-todo-react/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/a2sv-tasks-multi-repo/react-projects/task-todo-react/",
+    base: "/",
     plugins: [react()],
 });
